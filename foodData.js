c2636e22263b93d90958c831a0aca8000248210e
willingsyd <willingsyd9883@gmail.com>
2026-03-05 21:54:51 +1100

    Add files via upload

---
+++ foodData.js
@@ -0,0 +1,29 @@
+const foodDatabase = [
+    {
+        "name": "WWS Beef Mince 500g",
+        "category": "Solid",
+        "kcal": 974,
+        "pro": 18.1,
+        "carbs": 1,
+        "fiber": 0,
+        "price": 1.7
+    },
+    {
+        "name": "WWS Chicken Breast 500g",
+        "category": "Solid",
+        "kcal": 438,
+        "pro": 22.3,
+        "carbs": 0,
+        "fiber": 0,
+        "price": 1.45
+    },
+    {
+        "name": "WWS Lite Milk 2L",
+        "category": "Liquid",
+        "kcal": 189,
+        "pro": 3.4,
+        "carbs": 4.9,
+        "fiber": 0,
+        "price": 0.16
+    }
+];
